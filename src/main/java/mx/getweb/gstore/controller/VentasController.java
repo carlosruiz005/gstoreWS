734f3dde9a13b7df7153c69ed29b26b2dae99525
Carlos Ruiz <8752411+carlosruiz005@users.noreply.github.com>
2022-04-02 23:58:18 -0600

    Actualización de test
    - Se agregó un método de prueba para el test.
    - Se agregó el archivo de propiedades del test.

--- src/main/java/mx/getweb/gstore/controller/VentasController.java
+++ src/main/java/mx/getweb/gstore/controller/VentasController.java
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import mx.getweb.gstore.entity.Item;
@@ -18,7 +20,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -30,39 +31,64 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(ApiConstants.VentasControllerConstants.URL)
-@ApiResponses(value = {
-    @ApiResponse(responseCode = ApiConstants.RESPONSE_CODE_200, description = ApiConstants.RESPONSE_CODE_200_DESCRIPTION,
-            content = {
-                @Content(mediaType = ApiConstants.CONTENT_TYPE_JSON_APPLICATION)}),
-    @ApiResponse(responseCode = ApiConstants.RESPONSE_CODE_404, description = ApiConstants.RESPONSE_CODE_404_DESCRIPTION,
-            content = @Content)})
+@ApiResponses(
+        value = {
+            @ApiResponse(
+                    responseCode = ApiConstants.RESPONSE_CODE_200,
+                    description = ApiConstants.RESPONSE_CODE_200_DESCRIPTION,
+                    content = {
+                        @Content(mediaType = ApiConstants.CONTENT_TYPE_JSON_APPLICATION),}
+            ),
+            @ApiResponse(
+                    responseCode = ApiConstants.RESPONSE_CODE_404,
+                    description = ApiConstants.RESPONSE_CODE_404_DESCRIPTION,
+                    content = @Content
+            ),}
+)
 public class VentasController {
 
-    private static final Logger LOG = Logger.getLogger(VentasController.class.getName());
+    private static final Logger LOG = Logger.getLogger(
+            VentasController.class.getName()
+    );
 
     @Autowired
     private ItemService itemService;
 
     @GetMapping(ApiConstants.VentasControllerConstants.FIND_ALL_ITEMS_URL)
-    @Operation(summary = ApiConstants.VentasControllerConstants.FIND_ALL_ITEMS_URL_SUMMARY,
-            description = ApiConstants.VentasControllerConstants.FIND_ALL_ITEMS_URL_DESCRIPTION)
-    @ApiResponse(content = {
-        @Content(schema = @Schema(implementation = Item.class))})
+    @Operation(
+            summary = ApiConstants.VentasControllerConstants.FIND_ALL_ITEMS_URL_SUMMARY,
+            description = ApiConstants.VentasControllerConstants.FIND_ALL_ITEMS_URL_DESCRIPTION
+    )
+    @ApiResponse(
+            content = {
+                @Content(schema = @Schema(implementation = Item.class))}
+    )
     public ResponseEntity<List<Item>> findAllItems() {
         return new ResponseEntity(this.itemService.findAll(), HttpStatus.OK);
     }
 
-    @GetMapping(ApiConstants.VentasControllerConstants.FIND_ITEM_BY_BARCODE_ADDRESS_URL)
-    @Operation(summary = ApiConstants.VentasControllerConstants.FIND_ITEM_BY_BARCODE_ADDRESS_URL_SUMMARY,
-            description = ApiConstants.VentasControllerConstants.FIND_ITEM_BY_BARCODE_ADDRESS_URL_DESCRIPTION)
-    @ApiResponse(content = {
-        @Content(mediaType = ApiConstants.CONTENT_TYPE_JSON_APPLICATION)})
-    public ResponseEntity<Item> findItemByBarcode(@PathVariable("barcode") String barcode) {
+    @GetMapping(
+            ApiConstants.VentasControllerConstants.FIND_ITEM_BY_BARCODE_ADDRESS_URL
+    )
+    @Operation(
+            summary = ApiConstants.VentasControllerConstants.FIND_ITEM_BY_BARCODE_ADDRESS_URL_SUMMARY,
+            description = ApiConstants.VentasControllerConstants.FIND_ITEM_BY_BARCODE_ADDRESS_URL_DESCRIPTION
+    )
+    @ApiResponse(
+            content = {
+                @Content(mediaType = ApiConstants.CONTENT_TYPE_JSON_APPLICATION),}
+    )
+    public ResponseEntity<Item> findItemByBarcode(
+            @PathVariable("barcode") String barcode
+    ) {
         LOG.log(Level.INFO, "Consulta por codigo de barra: " + barcode);
         Item item = this.itemService.findByBarcode(barcode);
         if (item == null) {
             LOG.log(Level.WARNING, "Código no encontrado: " + barcode);
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No exite el item con código: " + barcode);
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "No exite el item con código: " + barcode
+            );
         }
         LOG.log(Level.INFO, "item: " + item);
         item.setCreated(null);
@@ -70,4 +96,10 @@ public class VentasController {
         return new ResponseEntity(item, HttpStatus.OK);
     }
 
+    @GetMapping("/alive")
+    public ResponseEntity<Map> alive() {
+        Map<String, String> m = new HashMap();
+        m.put("valor", "alive");
+        return ResponseEntity.ok(m);
+    }
 }
--- src/test/java/mx/getweb/gstore/GstoreApplicationTests.java
+++ src/test/java/mx/getweb/gstore/GstoreApplicationTests.java
@@ -1,13 +1,41 @@
 package mx.getweb.gstore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest()
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-test.properties")
 class GstoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mvc;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void isAlive() {
+        try {
+            mvc
+                    .perform(get("/ventas/alive"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType("application/json"))
+                    .andExpect(jsonPath("$.valor").value("alivee"));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
